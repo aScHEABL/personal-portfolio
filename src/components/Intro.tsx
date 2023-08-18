110b53774e3fb5d03f3b7d62f0a5e650ed3b3ec4
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-18 19:37:25 +0800

    fix: MediaQuery not hidden accordingly

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { 
     Container,
+    Flex,
     Box,
     Title,
     Text,
@@ -11,7 +12,7 @@ import {
 export default function Intro() {
     return (
         <>
-            <MediaQuery largerThan="768px" styles={{ display: 'none' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
                 <Container size="20rem">
                     <Title order={1} size="3.5rem">Hi, I'm Eric Trager.</Title>
                     <Title order={2} size="1.3rem" mb={48}>Student // Self-taught web developer</Title>
@@ -23,16 +24,18 @@ export default function Intro() {
                     HTML/CSS, Python, Node.js, MongoDB and more.</Text>
                 </Container>
             </MediaQuery>
-            <MediaQuery smallerThan="425px" styles={{ display: 'none' }}>
-                <Container size="100rem" px="8rem">
-                    <Title order={1} size="3.5rem">Hi, I'm Eric Trager.</Title>
-                    <Title order={2} size="1.3rem" mb={48}>Student // Self-taught web developer</Title>
-                    <Text>I am a third-year student studying Information Management 
-                        at <Anchor href="https://www.ntub.edu.tw/" target="_blank">NTUB</Anchor>. 
-                    Currently, I work as a MIS engineer intern
-                    at <Anchor href="https://www.feg.com.tw/en/home/index.aspx" target="_blank">Far Eastern Group</Anchor>.</Text>
-                    <Text>I develop quality frontend and backend applications and tools using Typescript, React, 
-                    HTML/CSS, Python, Node.js, MongoDB and more.</Text>
+            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+                <Container size="100rem" px="1rem">
+                    <Title order={1} size={70}>Hi, I'm Eric Trager.</Title>
+                    <Title order={2} size={20} mb={48}>Student // Self-taught web developer</Title>
+                    <Flex wrap="wrap">
+                        <Text size={18} styles={{ flex: "1 1 100%" }}>I am a third-year student studying Information Management 
+                            at <Anchor href="https://www.ntub.edu.tw/" target="_blank">NTUB</Anchor>. 
+                        Currently, I work as a MIS engineer intern
+                        at <Anchor href="https://www.feg.com.tw/en/home/index.aspx" target="_blank">Far Eastern Group</Anchor>.</Text>
+                        <Text size={18} styles={{ flex: "1 1 100%" }}>I develop quality frontend and backend applications and tools using Typescript, React, 
+                        HTML/CSS, Python, Node.js, MongoDB and more.</Text>
+                    </Flex>
                 </Container>
             </MediaQuery>
         </>
