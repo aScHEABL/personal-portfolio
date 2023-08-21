09e6574c4147400e65f40af2de407add5f1f90fa
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-21 17:31:49 +0800

    WIP: projectsShowcase component #0

---
+++ src/components/ProjectsShowcase.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+export default function ProjectsShowcase() {
+    return (
+        <>
+            <Container>
+                
+            </Container>
+        </>
+    )
+}
