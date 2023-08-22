7713ce237a04b02f39128ceb43c7103b50f64958
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-23 01:10:57 +0800

    WIP: Showcase #1

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,13 +1,35 @@
-import { Container, Flex, MediaQuery } from "@mantine/core";
+import { Container, Flex, MediaQuery, Title, Text, Anchor, Image } from "@mantine/core";
+import { v4 as uuid } from "uuid";
 
-export default function Showcase({ projects }) {
+const projects = [
+    {
+        name: <Title>Taster the bakery</Title>,
+        desc: <Text size={20}>This is an improved website from 
+        <Anchor href="https://tasters.imweb.me/">&nbsp;吃吃看Tasters</Anchor> 
+        &nbsp;at the request of the business owner</Text>,
+        img: <Image
+        src="src/assets/Screenshot_20230630_093002.png"
+        alt="a screenshot of a bakery website"
+        />
+    },
+    {
+        name: <Title>Weather App</Title>,
+        desc: <Text size={20}>A polish web app that displays current weather for any given city/region</Text>,
+        img: <Image
+        src="src/assets/Screenshot-2022-11-22.PNG"
+        alt="a screenshot of weather web app"
+        />
+    }
+]
+
+export default function Showcase() {
     return (
         <>
             <MediaQuery query="(max-width: 1024px) and (min-width: 300px)" styles={{ display: 'block' }}>
                 <Container sx={{ display: 'none' }}>
                     {projects.map((project) => {
                         return (
-                            <Flex wrap="wrap" px={20} py={40} rowGap={20}>
+                            <Flex key={uuid()} wrap="wrap" px={20} py={40} rowGap={20}>
                                 {project.name}
                                 {project.desc}
                                 {project.img}
