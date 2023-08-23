ddf87f110ec30796ba8b2c0b73a2f4849a331edc
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-23 21:07:43 +0800

    update: layout

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,4 +1,13 @@
-import { Container, Flex, MediaQuery, Title, Text, Anchor, Image } from "@mantine/core";
+import { Container, 
+    Flex, 
+    MediaQuery, 
+    Title, 
+    Text, 
+    Anchor, 
+    Image,
+    Box,
+
+} from "@mantine/core";
 import { v4 as uuid } from "uuid";
 
 const projects = [
@@ -14,12 +23,20 @@ const projects = [
     },
     {
         name: "Weather App",
-        desc: <div>A polish web app that displays current weather for any given city/region</div>,
+        desc: <div>A polish and sleek web app that displays current weather for any given city/region</div>,
         img: {
             src: "src/assets/Screenshot-2022-11-22.PNG",
             alt: "a screenshot of weather web app",
         }
-    }
+    },
+    {
+        name: "To do list app",
+        desc: <div>A to do list web app that let you add and edit task, dragging tasks and move them between the columns.</div>,
+        img: {
+            src: "src/assets/Screenshot 2023-08-21 at 11.26.39 PM.png",
+            alt: "a screenshot of to do list app"
+        }
+    },
 ]
 
 export default function Showcase() {
@@ -39,17 +56,72 @@ export default function Showcase() {
                                 src={project.img.src}
                                 alt={project.img.alt}
                                 />
-                                {/* {project.name}
-                                {project.desc}
-                                {project.img} */}
                             </Flex>
                         )
                     })}
                 </Container>
             </MediaQuery>
-            <MediaQuery query="(max-width: 1439px) and (min-width: 1024px)" styles={{ display: 'none' }}>
+            <MediaQuery query="(max-width: 1439px) and (min-width: 1024px)" styles={{ display: 'block' }}>
                     <Container sx={{ display: 'none' }}>
-
+                        {projects.map((project, index) => {
+                            if (index % 2 === 1) {
+                                return (
+                                    <Flex key={uuid()} px={20} py={40} rowGap={20}>
+                                        <Box sx={{
+                                            zIndex: "1",
+                                            position: "relative",
+                                            left: "4em",
+                                        }}>
+                                            <Title>
+                                                {project.name}
+                                            </Title>
+                                            <Box sx={(theme) => ({
+                                                margin: "1em 0 0 0",
+                                                padding: "1em 1em 1em 1em",
+                                                backgroundColor: theme.colors.gray[8],
+                                                borderRadius: "6px"
+                                            })}>
+                                                <Text size={20} p={12}>
+                                                    {project.desc}
+                                                </Text>
+                                            </Box>
+                                        </Box>
+                                        <Image
+                                            src={project.img.src}
+                                            alt={project.img.alt}
+                                        />
+                                    </Flex>
+                                )
+                            } else {
+                                return (
+                                    <Flex key={uuid()} px={20} py={40} rowGap={20}>
+                                        <Image
+                                        src={project.img.src}
+                                        alt={project.img.alt}
+                                        />
+                                        <Box sx={{
+                                            zIndex: "1",
+                                            position: "relative",
+                                            right: "4em"
+                                        }}>
+                                            <Title align="right">
+                                                {project.name}
+                                            </Title>
+                                            <Box sx={(theme) => ({
+                                                margin: "1em 0 0 0",
+                                                padding: "1em 1em 1em 1em",
+                                                backgroundColor: theme.colors.gray[8],
+                                                borderRadius: "6px"
+                                            })}>
+                                                <Text size={20} p={12}>
+                                                    {project.desc}
+                                                </Text>
+                                            </Box>
+                                        </Box>
+                                    </Flex>
+                                )
+                            }
+                        })}
                     </Container>
             </MediaQuery>
         </>
