716d97c5edfec990b3d9ece4f4594ddd2e065a89
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-25 00:16:56 +0800

    WIP: layout for mobile #0

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,11 +1,17 @@
-import { Box, Title } from "@mantine/core";
+import { 
+    Box,
+    Title,
+    Text,
+    Flex,
+    MediaQuery
+ } from "@mantine/core";
 import { v4 as uuid } from "uuid";
 
 const job_array = [
 {
-    company: "Far Eastern Group, ding & ding management consultants co. ltd",
-    jobTitle: "Management information intern, help desk",
-    Date: "Sep 2022 - Present"
+    company: "Far Eastern Group",
+    jobTitle: "MIS intern, Help desk",
+    date: "Sep 2022 - Present"
 },
 ]
 
@@ -15,9 +21,31 @@ export default function WorkExperience() {
             {
                 job_array.map((job) => {
                     return (
-                        <Box key={uuid()}>
-                            <Title>{job.company}</Title>
-                        </Box>
+                        <>
+                            <MediaQuery query="(max-width: 425px) and (min-width: 300px)" styles={{ display: 'block' }}>
+                                <Flex p={20} key={uuid()} wrap="wrap" 
+                                sx={(theme) => ({
+                                    display: 'none', 
+                                    borderBottom: "1px solid",
+                                    borderColor: theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.gray[6]
+                                })}>
+                                    <Title order={3} size={20}>
+                                        {job.company}
+                                    </Title>
+                                    <Text pb={8}>
+                                        {job.date}
+                                    </Text>
+                                    <Title order={4} size={18}>
+                                        {job.jobTitle}
+                                    </Title>
+                                </Flex>
+                            </MediaQuery>
+                            <MediaQuery query="(max-width: 767px) and (min-width: 424px)" styles={{ display: 'block' }}>
+                                <Flex p={20} key={uuid()} wrap="wrap">
+                                    
+                                </Flex>
+                            </MediaQuery>
+                        </>
                     )
                 })
             }
