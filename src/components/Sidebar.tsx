47ede82269cdf501eb57a3a9d1c315c0613a3191
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-25 17:27:10 +0800

    fix: zIndex to 9999

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,9 +13,14 @@ export default function Sidebar() {
     return (
         <>
             <MediaQuery query="(min-width: 993px)" styles={{ display: 'flex' }}>
-                <Flex direction="column" rowGap={16} sx={{ 
-                    display: "none", position: "fixed", paddingLeft: "1rem",
-                    paddingTop: "18rem" }}>
+                <Flex direction="column" rowGap={16}
+                     sx={{ 
+                            display: "none",
+                            zIndex: 9999,
+                            position: "fixed",
+                            paddingLeft: "1rem",
+                            paddingTop: "18rem",
+                        }}>
                     <ActionIcon size="xl" variant="transparent">
                         <BiLogoLinkedin size="2rem" />
                     </ActionIcon>
