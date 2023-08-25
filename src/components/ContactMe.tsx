c238973fa08ca7d87ef99efb05040b91c90bc3fa
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-25 23:56:50 +0800

    WIP: nanoid & mobile breakpoint #0

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -3,22 +3,29 @@ import {
     Flex,
     Title,
     Text,
+    MediaQuery,
  } from "@mantine/core";
 import { v4 as uuid } from "uuid";
+import { nanoid } from "nanoid";
 
 export default function ContactMe() {
     return (
-        <Container pos="relative" fluid mih="25rem" py={80}
-        sx={(theme) => ({
-            backgroundColor: theme.colors.gray[8],
-        })}>
-            <Title align="center">CONTACT ME 👋</Title>
-            <Flex pos="absolute" bottom="0" left="50%"
-            sx={{ 
-                transform: "translate(-50%, -50%)",
-                }}>
-                <Text>Current build: experiment-0@{uuid()} (Aug 25, 2023)</Text>
-            </Flex>
-        </Container>
+        <>
+            <MediaQuery query="(max-width: 425px)" styles={{ display: 'block' }}>
+                <Container pos="relative" fluid mih="25rem" py={80}
+                sx={(theme) => ({
+                    display: 'none',
+                    backgroundColor: theme.colors.gray[8],
+                })}>
+                    <Title align="center">CONTACT ME</Title>
+                    <Text size={12} pos="absolute" bottom="0" left="50%" w="80%" align="center"
+                    sx={{ 
+                        transform: "translate(-50%, -50%)",
+                        }}>
+                            Current build: {nanoid(10)} (Aug 25, 2023)
+                        </Text>
+                </Container>
+            </MediaQuery>
+        </>
     )
 }
