767d62001e2a70dcac61c50faa10eba2b6ad3b29
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-08-25 13:29:49 +0800

    update: large screen support & assign uuid

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -7,57 +7,117 @@ import {
     MediaQuery
  } from "@mantine/core";
 import { v4 as uuid } from "uuid";
+import { useEffect, useState, Fragment } from "react";
 
-const job_array = [
-{
-    company: "Far Eastern Group",
-    title: "MIS intern, Help desk",
-    durationDate: "Sep 2022 - Present"
-},
+interface jobs_prop {
+    company: string;
+    title: string;
+    durationDate: string;
+    id?: string
+}
+
+const jobs = [
+    {
+        company: "Far Eastern Group",
+        title: "MIS intern, Help desk",
+        durationDate: "Sep 2022 - Present"
+    },
+    {
+        company: "輝宇運動有限公司",
+        title: "Lifeguard",
+        durationDate: "Jan 2022 - Oct 2022"
+    }
 ]
 
 export default function WorkExperience() {
+    const [job_array, setJobs_array] = useState<jobs_prop[]>([]);
+    useEffect(() => {
+        setJobs_array(jobs.map((job) => ({ ...job, id: uuid() })));
+    }, [])
     return (
         <Box>
-            {
-                job_array.map((job) => {
-                    return (
-                        <>
-                            <MediaQuery query="(max-width: 424px) and (min-width: 300px)" styles={{ display: 'block' }}>
-                                <Flex p={20} key={uuid()} wrap="wrap" 
-                                sx={(theme) => ({
-                                    display: 'none', 
-                                    borderBottom: "1px solid",
-                                    borderColor: theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.gray[6]
-                                })}>
-                                    <Title order={3} size={20}>
-                                        {job.company}
-                                    </Title>
-                                    <Text pb={8}>
-                                        {job.durationDate}
-                                    </Text>
-                                    <Title order={4} size={18}>
-                                        {job.title}
-                                    </Title>
-                                </Flex>
-                            </MediaQuery>
-                            <MediaQuery query="(max-width: 767px) and (min-width: 426px)" styles={{ display: 'block' }}>
-                                <Container size="30rem" p={40} sx={{ display: 'none' }}>
-                                    <Flex wrap="wrap" justify="space-between">
+            <MediaQuery query="(max-width: 340px) and (min-width: 300px)" styles={{ display: 'block' }}>
+                <Box sx={{ display: 'none' }}>
+                    {
+                        job_array.map((job) => {
+                            return (
+                                        <Flex p={20} key={job.id} wrap="wrap" 
+                                        sx={(theme) => ({
+                                            '&:not(:last-child)': {
+                                                borderBottom: '1px solid',
+                                                borderColor: theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.gray[8]
+                                            }
+                                        })}>
+                                            <Title order={3} size={20}>
+                                                {job.company}
+                                            </Title>
+                                            <Text pb={8}>
+                                                {job.durationDate}
+                                            </Text>
+                                            <Title order={4} size={18}>
+                                                {job.title}
+                                            </Title>
+                                        </Flex>
+                            )
+                        })
+                    }
+                </Box>
+            </MediaQuery>
+            <MediaQuery query="(max-width: 767px) and (min-width: 341px)" styles={{ display: 'block' }}>
+                <Container size="30rem" px={40} pb={40} sx={{ display: 'none' }}>
+                    {
+                        job_array.map((job) => {
+                            return (
+                                <Fragment key={job.id}>
+                                    <Flex py={20} wrap="wrap" justify="space-between"
+                                    sx={(theme) => ({ 
+                                        '&:not(:last-child)': {
+                                            borderBottom: '1px solid',
+                                            borderColor: theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.gray[8]
+                                        }
+                                    })}>
                                         <Title order={3}>
                                             {job.company}
                                         </Title>
                                         <Text>
                                             {job.durationDate}
                                         </Text>
+                                        <Title order={4} sx={{ flex: "1 1 100%" }}>{job.title}</Title>
                                     </Flex>
-                                    <Title order={4}>{job.title}</Title>
-                                </Container>
-                            </MediaQuery>
-                        </>
-                    )
-                })
-            }
+                                </Fragment>
+                            )
+                        })
+                    }
+                </Container>
+            </MediaQuery>
+            <MediaQuery query="(max-width: 2560px) and (min-width: 768px)" styles={{ display: 'block' }}>
+                <Container size="60rem" pb={40} px={100} 
+                    sx={(theme) => ({ 
+                        display: 'none',
+                        '&:not(:last-child)': {
+                            border: '1px solid',
+                            borderColor: theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.gray[8]
+                        }
+                    })}>
+                    {
+                        job_array.map((job) => {
+                            return (
+                                <Fragment key={job.id}>
+                                    <Flex py={20} wrap="wrap" justify="space-between">
+                                        <Title order={3} size={25}>
+                                            {job.company}
+                                        </Title>
+                                        <Text size={20}>
+                                            {job.durationDate}
+                                        </Text>
+                                    </Flex>
+                                    <Title order={4} size={20}>{job.title}</Title>
+                                </Fragment>
+                            )
+                        })
+                    }
+                </Container>
+            </MediaQuery>
         </Box>
     )
 }
