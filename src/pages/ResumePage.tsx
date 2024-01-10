70c5880f9cf98c1bfa704fb19ed8235b69982838
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-01-10 16:56:47 +0800

    update: remove react-pdf & code refactor

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,23 +1,17 @@
-import { Container, ColorScheme } from "@mantine/core";
-import PDF from "../components/PDF";
+import { Container} from "@mantine/core";
 import resume from "../assets/pdf/resume.pdf"
-import { useEffect } from "react";
-import { useLocalStorage, useColorScheme } from "@mantine/hooks";
+import ColorToggle from "../components/ColorToggle";
 
 export default function ResumePage() {
-    const preferredColorScheme = useColorScheme();
-    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-        key: 'mantine-color-scheme',
-        defaultValue: preferredColorScheme,
-        getInitialValueInEffect: true,
-    })
-    useEffect(() => {
-        console.log("test");
-        setColorScheme("dark");
-    }, [])
+
     return (
-        <Container size="xs">
-            <PDF pdfFile={resume} />
-        </Container>
+        <>
+            <ColorToggle />
+            <Container h="calc(100vh - 86px)">
+                <object data={resume} type="application/pdf" width="100%" height="100%">
+                    <p>Alternative text - include a link <a href={resume}>to the PDF!</a></p>
+                </object>
+            </Container>
+        </>
     )
 }
